a421e0417c49ea11212835abb1ad4f7220163441
Alvaro <alvalfric@alum.us.es>
2021-05-24 15:13:41 +0200

    Updated Health model for request of changes #130

--- src/main/java/org/springframework/samples/petclinic/model/Health.java
+++ src/main/java/org/springframework/samples/petclinic/model/Health.java
@@ -1,12 +1,7 @@
 package org.springframework.samples.petclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 import lombok.Data;
 
-@Entity
-@Table(name="adoptionApplication")
 @Data
 public class Health extends BaseEntity{
 
